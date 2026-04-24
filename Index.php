6661b49546c7b2b9fc92ad5b87a0bc1e0025efc1
ia901092 <ia901092@gmail.com>
2026-04-24 09:51:38 +0200

    lincese och php xml data för url

--- Index.php
+++ Index.php
@@ -9,3 +9,21 @@
     <h1>The Daily Reader</h1>
 </body>
 </html>
+<?php
+    $xmlFilePapper = file_get_contents('https://wwwlab.webug.se/examples/XML/articleservice/papers/');
+    $domPapers = new DOMDocument;
+    $domPapers->preserveWhiteSpace = FALSE;
+    $domPapers->loadXML($xmlFilePapper);
+
+    // default paper if nothing is posted
+    if (isset($_POST['sender'])) {
+        $paper = $_POST['sender'];
+    } else {
+        $paper = "Morning_Edition";
+    }
+
+    $xmlArticles = file_get_contents("https://wwwlab.webug.se/examples/XML/articleservice/articles?paper=" . $paper);
+    $domArticles = new DOMDocument;
+    $domArticles->preserveWhiteSpace = FALSE;
+    $domArticles->loadXML($xmlArticles);
+?> 
